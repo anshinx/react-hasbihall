a70f0fc0d571b2d20832db40293b5d0ef3f44b1d
anshinx <yenersg.58@gmail.com>
2021-08-07 16:49:22 +0300

    deploy

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
